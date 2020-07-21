26de596bdf3fa15d23057b37f9891a9822b02936
Reza Rinaldi <rez4rinaldi@gmail.com>
2020-07-21 14:46:00 +0700

    initial commit

---
+++ src/common/types/settings.ts
@@ -0,0 +1,39 @@
+export type Settings = {
+  logo: string;
+  logoUrl: string;
+  name: string;
+  useTables: boolean;
+  useKitchen: boolean;
+  usePromotions: boolean;
+  useFiscalization: boolean;
+  isAllowPrintCheck: boolean;
+  useFastPay: boolean;
+  wifiName: string;
+  wifiPass: string;
+  tipAmount: number;
+  predictions: string[];
+  printReceiptNumber: boolean;
+  printAddress: boolean;
+  printPrediction: boolean;
+  printReceiptDuplicate: boolean;
+  printCheckDuplicate: boolean;
+  printSumWeightOnReceipt: boolean;
+  printReceipt: boolean;
+  printReceiptByDefault: boolean;
+  printWifi: boolean;
+  printReceiptComment: boolean;
+  lang: string;
+  timezone: string;
+  currencyCodeIso: string;
+  currency: string;
+  currencySymbol: string;
+  paymentMethods: PaymentMethod[];
+};
+
+export type PaymentMethod = {
+  paymentMethodId: number;
+  title: string;
+  icon: string;
+  color: string;
+  isActive: boolean;
+};
---
+++ src/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { createHashHistory } from 'history';
+import { AppContextProvider } from 'common/hooks';
+import { Routes } from 'common/const';
+import Terminal from './terminal/terminal';
+import Admin from './admin/admin';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+const hashHistory = createHashHistory();
+
+const NoMatch = () => (
+  <div>
+    <h1>404</h1>
+    Page Not Found
+  </div>
+);
+
+ReactDOM.render(
+  <AppContextProvider>
+    <Router history={hashHistory}>
+      <Switch>
+        <Route path={Routes.Terminal}>
+          <Terminal />
+        </Route>
+        <Route path={Routes.AdminDashboard}>
+          <Admin />
+        </Route>
+        <Redirect strict from="/" to={Routes.Terminal} />
+        <Route render={NoMatch} />
+      </Switch>
+    </Router>
+  </AppContextProvider>,
+  document.getElementById('root'),
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
